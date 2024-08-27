f910ec4a74a39e4164109e0a03338ad3876e8721
Hans Spieß <spiess@demos-deutschland.de>
2024-08-27 09:23:38 +0200

    feat: add commonjs support

---
+++ eslint.config.cjs
@@ -0,0 +1,85 @@
+const globals = require('globals');
+const eslintJs = require('@eslint/js');
+const pluginVue = require('eslint-plugin-vue');
+const pluginVueA11y = require('eslint-plugin-vuejs-accessibility');
+const stylisticJs = require('@stylistic/eslint-plugin-js');
+const stylisticTs = require('@stylistic/eslint-plugin-ts');
+const tsEslint = require('typescript-eslint');
+
+module.exports = [
+  eslintJs.configs.recommended,
+  ...tsEslint.configs.recommended,
+  ...pluginVue.configs['flat/recommended'],
+  ...pluginVueA11y.configs['flat/recommended'],
+  {
+    ignores: [
+      "**/node_modules/**"
+    ]
+  },
+  {
+    files: ["**/*.js", "**/*.ts", "**/*.vue"],
+    languageOptions: {
+      globals: globals.browser,
+      parserOptions: {
+        parser: tsEslint.parser
+      }
+    },
+    plugins: {
+      '@stylistic/js': stylisticJs,
+      '@stylistic/ts': stylisticTs,
+      'typescript': tsEslint.plugin,
+      'vue': pluginVue,
+      'vuejs-accessibility': pluginVueA11y
+    },
+    rules: {
+      'generator-star-spacing': 'off',
+      'multiline-comment-style': 'error',
+      'sort-imports': ['error', { 'ignoreCase': true }],
+      '@stylistic/js/object-curly-spacing': ['error', 'always'],
+      '@stylistic/js/indent': ['error', 2],
+      '@stylistic/ts/indent': ['error', 2],
+      '@stylistic/ts/object-curly-spacing': ['error', 'always'],
+      'vue/html-closing-bracket-newline': ['error', {
+        'singleline': 'never',
+        'multiline': 'never'
+      }],
+      'vue/object-curly-spacing': ['error', 'always'],
+      'vue/order-in-components': ['error', {
+        'order': [
+          'el',
+          'name',
+          'parent',
+          'functional',
+          ['delimiters', 'comments'],
+          ['components', 'directives'],
+          'extends',
+          'mixins',
+          'inheritAttrs',
+          'model',
+          ['props', 'propsData'],
+          'data',
+          'computed',
+          'watch',
+          'methods',
+          'LIFECYCLE_HOOKS',
+          ['template', 'render'],
+          'renderError'
+        ]
+      }],
+      'vue/block-order': ['error', {
+        'order': ['docs', 'template', 'script:not([setup])', 'script[setup]', 'style']
+      }],
+      'vue/define-macros-order': ['error', {
+        'order': ['defineProps', 'defineEmits'],
+        'defineExposeLast': true
+      }],
+      'vue/v-slot-style': ['error', {
+        'atComponent': 'longform',
+        'default': 'longform',
+        'named': 'longform'
+      }],
+      'vue/multi-word-component-names': 'warn',
+      'vuejs-accessibility/label-has-for': 'warn'
+    }
+  }
+]
